20e1bc6d854653bfa7508911c0c644f6281205c5
chixinaiya <1251512427@qq.com>
2020-04-22 16:31:11 +0800

    百度地图

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,12 +1,40 @@
-import React,{Component} from 'react'
-class Map extends Component{
+import React, { Component } from "react";
+import "./index.scss";
+import { NavBar, Icon } from "antd-mobile";
 
-  render(){
-    return(
-     <div>
-      Map
-     </div>
-    )
+class Map extends Component {
+  componentDidMount() {
+    this.initMap();
+  }
+
+  // 初始化地图
+  initMap = () => {
+    const { BMap } = window;
+    // console.log(BMap);
+
+    // 创建地图实例
+    const map = new BMap.Map("container");
+    // 地图定位的经纬度位置设置
+    let point = new BMap.Point(116.404, 39.915);
+    // 设置地图的位置和缩放级别
+    map.centerAndZoom(point, 15);
+  };
+
+  render() {
+    return (
+      <div className="mapBox">
+        {/* 地图导航栏 */}
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left" />}
+          onLeftClick={() => this.props.history.goBack()}
+        >
+          地图找房
+        </NavBar>
+        {/* 地图 */}
+        <div id="container"></div>
+      </div>
+    );
   }
 }
-export default Map
+export default Map;
