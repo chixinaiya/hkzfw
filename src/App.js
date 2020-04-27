cd7d9b178a5c280986183596eb50f3122df87d57
chixinaiya <1251512427@qq.com>
2020-04-27 15:49:21 +0800

    登录基础布局

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import CityList from "./pages/CityList";
 import Map from "./pages/Map";
 import NotFound from "./pages/NotFound";
 import HouseDetail from "./components/HouseDetail";
-
+import Login from "../src/pages/Login/index";
 function App() {
   return (
     <Router>
@@ -24,6 +24,8 @@ function App() {
           <Route path="/home" component={Home} />
           <Route path="/cityList" component={CityList} />
           <Route path="/map" component={Map} />
+          {/* 登录路由 */}
+          <Route path="/login" component={Login} />
           {/* // 配置路由 */}
           <Route path="/detail/:id" component={HouseDetail} />
           {/* 配置404页面 */}
